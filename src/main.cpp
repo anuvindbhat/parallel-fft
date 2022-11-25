9848776991682d3f018dbb913dc4d66da79fd425
Anuvind Bhat <anuvind.bhat@gmail.com>
2022-11-24 22:01:46 -0500

    formatting

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,8 @@ int main() {
     int n = 1 << 20;
     std::vector<double> vec(n);
     std::iota(vec.begin(), vec.end(), 0);
-    std::cout << "Initial: " << std::vector(vec.begin(), vec.begin() + 10) << "\n";
+    std::cout << "Initial: " << std::vector(vec.begin(), vec.begin() + 10)
+              << "\n";
     auto cvec = to_complex(vec);
     // std::cout << "Before FFT: " << cvec << "\n";
     fft_rec<false>(cvec);
@@ -20,7 +21,8 @@ int main() {
     fft_rec<true>(cvec);
     // std::cout << "After FFT-inv: " << cvec << "\n";
     vec = to_reals(cvec);
-    std::cout << "Final: " << std::vector(vec.begin(), vec.begin() + 10) << "\n";
+    std::cout << "Final: " << std::vector(vec.begin(), vec.begin() + 10)
+              << "\n";
   }
   return 0;
 }
