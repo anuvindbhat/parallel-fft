2b4d17a7010d6167dd575444ff7e3dd58be3d260
Anuvind Bhat <anuvind.bhat@gmail.com>
2022-12-01 16:26:13 -0500

    Microopt

--- src/fft1d.cpp
+++ src/fft1d.cpp
@@ -200,7 +200,8 @@ template <bool inverse> void fft_iter(std::vector<std::complex<double>> &vec) {
         // arithmetic
         // this is slower for low thread counts but scales better at high
         // thread counts
-        int32_t j = k - k % len;
+        // k & (len - 1) is k % len when len is a power of 2
+        int32_t j = k - (k & (len - 1));
         int32_t i = (k - j) / 2;
         loop_body(len, j, i);
       }
