a8da696027b125f4fb972949168f88eb65d5bb28
Anuvind Bhat <anuvind.bhat@gmail.com>
2022-12-20 01:58:42 -0500

    remove using declaration

--- src/timer.h
+++ src/timer.h
@@ -13,24 +13,23 @@ public:
   Timer(std::string name)
       : m_name(std::move(name)), m_beg(std::chrono::steady_clock::now()) {}
   ~Timer() {
-    using namespace std;
-    auto end = chrono::steady_clock::now();
-    auto dur = chrono::duration_cast<T>(end - m_beg);
+    auto end = std::chrono::steady_clock::now();
+    auto dur = std::chrono::duration_cast<T>(end - m_beg);
     std::string suffix;
-    if constexpr (std::is_same_v<T, chrono::nanoseconds>) {
+    if constexpr (std::is_same_v<T, std::chrono::nanoseconds>) {
       suffix = "ns";
-    } else if constexpr (std::is_same_v<T, chrono::microseconds>) {
+    } else if constexpr (std::is_same_v<T, std::chrono::microseconds>) {
       suffix = "us";
-    } else if constexpr (std::is_same_v<T, chrono::milliseconds>) {
+    } else if constexpr (std::is_same_v<T, std::chrono::milliseconds>) {
       suffix = "ms";
-    } else if constexpr (std::is_same_v<T, chrono::seconds>) {
+    } else if constexpr (std::is_same_v<T, std::chrono::seconds>) {
       suffix = "s";
     } else {
       // static_assert false inside if constexpr is apparently ill formed
       // https://stackoverflow.com/questions/53945490
       static_assert(always_false<T>, "Unsupported duration");
     }
-    cout << m_name << ": " << dur.count() << " " << suffix << "\n";
+    std::cout << m_name << ": " << dur.count() << " " << suffix << "\n";
   }
 
 private:
